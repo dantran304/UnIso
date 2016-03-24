119c36ff4b747927d057b87ed188db9a3b90fdf2
dantran <dantrandinhh@gmail.com>
2016-03-24 21:10:35 +0700

    map + character

---
+++ Assets/Script/CameraMoving.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoving : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        //tinh khoang cach chenh lech giua camera va player
+        offset = gameObject.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        gameObject.transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float playerSpeed;   // tốc độ ban đầu
+    public float acceleration;  // gia tốc
+    Animator anim;
+    Rigidbody rigid;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        AutoMove();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rigid.AddForce(0, 500, 0);
+        }
+    }
+
+    void AutoMove()
+    {
+        playerSpeed = playerSpeed * acceleration;
+        transform.Translate(0, 0, playerSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+
+    }
+}
